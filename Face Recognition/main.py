ae3bc9fb677795c4f03e0a3c3d2c12e976ce96d4
unknown <vinay55bansal@gmail.com>
2020-05-27 01:19:51 +0530

    Face

--- Face Recognition/main.py
+++ Face Recognition/main.py
@@ -18,7 +18,7 @@ while True:
 	face_section=0
     
 
-	faces = face_cascade.detectMultiScale(gray, 1.3, 5) #find face
+	faces = face_cascade.detectMultiScale(frame, 1.3, 5) #find face
 	faces=sorted(faces, key=lambda f:f[2]*f[3]) #find largest face
 
 	for (x,y,w,h) in faces[:-1]:
@@ -28,7 +28,7 @@ while True:
 		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2) #make yellow rectangle on largest
 
 		offset=10
-		face_section=gray[y-offset:y+h+offset,x-offset:x+w+offset]
+		face_section=frame[y-offset:y+h+offset,x-offset:x+w+offset]
 		face_section=cv2.resize(face_section,(100,100))
 		if(skip%10==0 and len(face_section)!=0):
 			face_data.append(face_section)
@@ -51,7 +51,7 @@ face_data=np.asarray(face_data)
 face_data=face_data.reshape((face_data.shape[0],-1))
 print(face_data.shape)
 
-# #save
+#save
 np.save(dataset_path+file_name+".npy",face_data)
 print("face saved")
 cap.release()
