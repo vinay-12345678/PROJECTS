b09ca4afebc94596202c936112f8b2483d65420d
unknown <vinay55bansal@gmail.com>
2020-06-08 00:20:28 +0530

    Voice assist

--- Voice typing/voice.py
+++ Voice typing/voice.py
@@ -1,5 +1,21 @@
 import speech_recognition as sr
 import winsound
+import pyttsx3
+
+def speak(words):
+    engine.say(words)
+    engine.runAndWait()
+
+def goto(linenum):
+    global line
+    line=linenum
+
+engine=pyttsx3.init("sapi5")
+voices=engine.getProperty("voices")
+# print(voices)
+engine.setProperty("voice",voices[1].id)
+
+
 r=sr.Recognizer()
 
 while True:
@@ -9,22 +25,43 @@ while True:
         duration=1000
         winsound.Beep(frequency,duration)
 
-        print("SPEAK :  ")
+        speak("speak")
+        print()
+        print("LISTENING....   ")
         audio=r.listen(source)
 
         try:
             text=r.recognize_google(audio)
-            print("I HEARD : ",text)
-            print("DO YOU WANT TO SAVE IT?","YES=Y","NO=N","QUIT=Q",sep="\n")
-            a=input()
-            if(a=='Y'):
-                with open ("audio.txt",'a') as fil:
-                    fil.write(text+'\n')
-            elif(a=='Q'):
+
+            speak("i heard you saying")
+
+            print("YOU :",text)
+
+            speak("Do you want to save it.")
+            speak("Say, Yes,no,or quit")
+
+            # print("YES=Y","NO=N","QUIT=Q",sep="\n")
+            print("LISTENING...   ")
+            audio=r.listen(source)
+            
+            line=1
+            try:
+                a=r.recognize_google(audio)
+                if(a=='yes' or a=='yes yes'):
+                    with open ("audio.txt",'a') as fil:
+                        fil.write(text+'\n')
+                    speak("saved successfully")
+                    break
+                elif(a=='no' or a=='no no'):
+                    continue
+                else:
+                    break
+
+            except:
+                speak("Can't recognize, Please try again!!")
                 break
-            else:
-                continue
+
         except:
-            print("Can't recognize, Please try again!!")
+            speak("Can't recognize, Please try again!!")
 
     
